0832508ca79222539fa6b70053b27353ff91f955
Alexis Ligon <aligon17@gmail.com>
2021-05-03 14:07:23 -0400

    reset css, file links, and pseudo code

---
+++ script.js
@@ -0,0 +1,11 @@
+//PSEUDO CODE
+//i need to create a card with a form to search for a city
+//the search needs to fetch data from the weather api
+//the data needs to be displayed on the side of the page
+//the display needs to include the name of the city, the date, an icon of the type of weather
+//for example a sun or a cloud or rain or snow
+//temp, wind, humidity, uv index
+//display the 5 day forecast
+// each day forecast has the date, a weather icon, temp, wind, and humidity
+// the most recent city search needs to display as a clickable link to easily access the most
+//recently searched weather
