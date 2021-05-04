4c734b8e36f8e33445a8dd3eaa03767f46a3b925
Alexis Ligon <aligon17@gmail.com>
2021-05-04 17:00:38 -0400

    added 5 day forecast

--- script.js
+++ script.js
@@ -9,18 +9,28 @@
 // each day forecast has the date, a weather icon, temp, wind, and humidity
 // the most recent city search needs to display as a clickable link to easily access the most
 //recently searched weather
-var jaxWeather = 'http://api.openweathermap.org/data/2.5/weather?q=Jacksonville,FLuk&APPID=2cd8c8262bad3d97fbc4eedd4e5664c4';
-
+var jaxWeather = 'http://api.openweathermap.org/data/2.5/weather?q=Jacksonville,FLuk&units=imperial&APPID=2cd8c8262bad3d97fbc4eedd4e5664c4';
+//single day weather forecast
 fetch (jaxWeather)
 .then(function(response){
     return response.json();
 })
 .then(function(data){
-    console.log(data);
+    console.log('data: ', data);
     console.log('name: ', data.name);
     console.log('main', data.main);
    console.log('temp: ', data.main.temp, 'degrees??');
    console.log('wind: ', data.wind.speed, 'mph')
    console.log('humidity: ', data.main.humidity, '%');
+})
+
+//5 day weather forecast
+fetch ('http://api.openweathermap.org/data/2.5/find?q=Raleigh&units=imperial&appid=12ab451d86c37bd1bbaa8df17ff823aa')
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log('data: ', data);
+    
 })
 
