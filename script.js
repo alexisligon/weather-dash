9247f2b59bb8a02168f249f31b543c86276f36e1
Alexis Ligon <aligon17@gmail.com>
2021-05-04 14:44:34 -0400

    console log fetching weather data

--- script.js
+++ script.js
@@ -9,3 +9,18 @@
 // each day forecast has the date, a weather icon, temp, wind, and humidity
 // the most recent city search needs to display as a clickable link to easily access the most
 //recently searched weather
+var jaxWeather = 'http://api.openweathermap.org/data/2.5/weather?q=Jacksonville,FLuk&APPID=2cd8c8262bad3d97fbc4eedd4e5664c4';
+
+fetch (jaxWeather)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    console.log('name: ', data.name);
+    console.log('main', data.main);
+   console.log('temp: ', data.main.temp, 'degrees??');
+   console.log('wind: ', data.wind.speed, 'mph')
+   console.log('humidity: ', data.main.humidity, '%');
+})
+
